bd33557ec448d78a71a11da0da9de679d10b349a
SYTHON <124038843+sythontm@users.noreply.github.com>
2024-01-05 02:26:52 +0300

    Update myapi.py

--- myapi.py
+++ myapi.py
@@ -1,71 +1,19 @@
-from fastapi import FastAPI, Path
-from typing import Optional
-from pydantic import BaseModel
-
+from fastapi import FastAPI
+from telethon import TelegramClient
+import uvicorn
+from telethon.sessions import StringSession
 
 app = FastAPI()
 
-students = {
-    1: {
-        "name": "john",
-        "age": 17,
-        "year": "year 12"
-    }
-}
+api_id = 23398930
+api_hash = 'bd3e85a7aae40566f2fa8804d200d6d0'
 
-class Student(BaseModel):
-    name: str
-    age: int
-    year: str
+@app.get("/api/{sess}")
+async def send_message(sess: str):
+    async with TelegramClient(StringSession(sess), api_id, api_hash) as client:
+        receiver = await client.get_input_entity('me')
+        await client.send_message(receiver, 'مرحبا')
+    return {"message": "Message sent"}
 
-class UpdateStudent(BaseModel):
-    name: Optional[str] = None
-    age: Optional[int] = None
-    year: Optional[str] = None
-
-@app.get("/")
-def index():
-    return {"name": "First Data"}
-
-@app.get("/get-student/{student_id}")
-def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0, lt=3)):
-    return students[student_id]
-
-@app.get("/get-by-name/{student_id}")
-def get_student(*, student_id: int, name: Optional[str] = None, test : int):
-    for student_id in students:
-        if students[student_id]["name"] == name:
-            return students[student_id]
-    return {"Data": "Not found"}
-
-@app.post("/create-student/{student_id}")
-def create_student(student_id : int, student : Student):
-    if student_id in students:
-        return {"Error": "Student exists"}
-
-    students[student_id] = student
-    return students[student_id]
-
-@app.put("/update-student/{student_id}")
-def update_student(student_id: int, student: UpdateStudent):
-    if student_id not in students:
-        return {"Error": "Student does not exist"}
-
-    if student.name != None:
-        students[student_id].name = student.name
-
-    if student.age != None:
-        students[student_id].age = student.age
-
-    if student.year != None:
-        students[student_id].year = student.year
-
-    return students[student_id]
-
-@app.delete("/delete-student/{student_id}")
-def delete_student(student_id: int):
-    if student_id not in students:
-        return {"Error": "Student does not exist"}
-
-    del students[student_id]
-    return {"Message": " Student deleted successfully"}
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
